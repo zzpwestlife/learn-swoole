a3ab283ab249851202e61e61cae25482c6ce0de8
ZouZhipeng <zzpwestlife@gmail.com>
2019-11-11 22:03:17 +0800

    init

---
+++ app/Http/Controllers/SwooleController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class SwooleController extends Controller
+{
+
+    public function server()
+    {
+        //创建 Server 对象，监听 127.0.0.1:9501 端口
+        $server = new Swoole\Server("127.0.0.1", 9501);
+
+        $server->set(array(
+            'reactor_num' => 2, //reactor thread num
+            'worker_num' => 8,    //worker process num
+            'max_request' => 5000,
+        ));
+
+        // 监听连接进入事件
+        // $fd 客户端连接的唯一标志
+        $server->on('Connect', function ($server, $fd, $reactor_id) {
+            echo "Client: Connect.\n" . ', fd: ' . $fd . ' , reactor_id: ' . $reactor_id;
+        });
+
+        // 监听数据接收事件
+        $server->on('Receive', function ($server, $fd, $reactor_id, $data) {
+            $server->send($fd, "Server: " . $data . ', fd: ' . $fd . ' , reactor_id: ' . $reactor_id);
+        });
+
+        // 监听连接关闭事件
+        $server->on('Close', function ($server, $fd) {
+            echo "Client: Close.\n";
+        });
+
+        // 启动服务器
+        $server->start();
+    }
+}
